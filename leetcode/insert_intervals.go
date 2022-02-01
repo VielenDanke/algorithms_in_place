3ebe9e48c6f8dd0884c549289025e229fe6d77fa
= <vielendanke1991@gmail.com>
2022-02-01 08:17:15 +0600

    merge intervals problem

---
+++ leetcode/insert_intervals.go
@@ -0,0 +1,33 @@
+package leetcode
+
+import (
+	"math"
+	"sort"
+)
+
+/*
+1. Iterate over the intervals
+2. if interval[i] covers new interval - return intervals itself
+3. if interval covers start - take interval[i][0]
+4. if interval covers end - take interval[i][1]
+5. store the idx of start and idx of end interval - cut all and insert new interval and return intervals
+*/
+
+func InsertIntervals(intervals [][]int, newInterval []int) [][]int {
+	if len(intervals) == 0 {
+		return [][]int{newInterval}
+	}
+	intervals = append(intervals, newInterval)
+	sort.Slice(intervals, func(i, j int) bool {
+		return intervals[i][0] < intervals[j][0]
+	})
+	resultIntervals := make([][]int, 0)
+	for _, v := range intervals {
+		if len(resultIntervals) == 0 || (resultIntervals[len(resultIntervals)-1][1] < v[0]) {
+			resultIntervals = append(resultIntervals, v)
+		} else {
+			resultIntervals[len(resultIntervals)-1][1] = int(math.Max(float64(resultIntervals[len(resultIntervals)-1][1]), float64(v[1])))
+		}
+	}
+	return resultIntervals
+}
--- main.go
+++ main.go
@@ -2,10 +2,22 @@ package main
 
 import (
 	"fmt"
-	"github.com/vielendanke/preparation/greedy_algorithms/easy"
+	"github.com/vielendanke/preparation/leetcode"
 )
 
 func main() {
-	arr := []int{3, 2, 1, 2, 6}
-	fmt.Printf("Result: %d\n", easy.MinimumWaitingTime(arr))
+	result := leetcode.InsertIntervals([][]int{{1, 2}, {3, 5}, {6, 7}, {8, 10}, {12, 16}}, []int{4, 8})
+
+	for _, v := range result {
+		print("{")
+		for k, val := range v {
+			if k != len(v)-1 {
+				fmt.Printf("%d, ", val)
+			} else {
+				fmt.Print(val)
+			}
+		}
+		print("}")
+		println()
+	}
 }
