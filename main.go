720922d7d2ce69a37dc19ade3a06819e1b749bd0
= <vielendanke1991@gmail.com>
2022-02-02 20:54:29 +0600

    algorithms

--- main.go
+++ main.go
@@ -2,22 +2,11 @@ package main
 
 import (
 	"fmt"
-	"github.com/vielendanke/preparation/leetcode"
+	"github.com/vielendanke/preparation/greedy_algorithms/easy"
 )
 
 func main() {
-	result := leetcode.InsertIntervals([][]int{{1, 2}, {3, 5}, {6, 7}, {8, 10}, {12, 16}}, []int{4, 8})
+	b := easy.TandemBicycle([]int{5, 5, 3, 9, 2}, []int{3, 6, 7, 2, 1}, false)
 
-	for _, v := range result {
-		print("{")
-		for k, val := range v {
-			if k != len(v)-1 {
-				fmt.Printf("%d, ", val)
-			} else {
-				fmt.Print(val)
-			}
-		}
-		print("}")
-		println()
-	}
+	fmt.Printf("Result: %d\n", b)
 }
