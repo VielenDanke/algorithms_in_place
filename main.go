af3f33f61ce7bf3e3e6f204deaca5fe9e3992fa4
Vladislav Dankevich <vielendanke1991@gmail.com>
2022-02-05 19:07:21 +0600

    algo

--- main.go
+++ main.go
@@ -1,4 +1,7 @@
 package main
 
+import "github.com/vielendanke/preparation/backtracking"
+
 func main() {
+	backtracking.Combine(4, 2)
 }
---
+++ sorting/bubble_sort.go
@@ -0,0 +1,15 @@
+package sorting
+
+func BubbleSort(array []int) []int {
+	isSorted := false
+	for !isSorted {
+		isSorted = true
+		for i := 0; i < len(array)-1; i++ {
+			if array[i] > array[i+1] {
+				array[i], array[i+1] = array[i+1], array[i]
+				isSorted = false
+			}
+		}
+	}
+	return array
+}
