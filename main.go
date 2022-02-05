fd0f3660fb1be79f7570642852b72527c84e3379
Vladislav Dankevich <vielendanke1991@gmail.com>
2022-02-05 14:32:23 +0600

    algorithms

--- main.go
+++ main.go
@@ -1,11 +1,4 @@
 package main
 
-import (
-	"fmt"
-	"github.com/vielendanke/preparation/sberbank"
-	"os"
-)
-
 func main() {
-	fmt.Fprintf(os.Stdout, "Result: %d\n", sberbank.GetResult2([]int{3, 2, 1, 4}, []int{1, 2, 3, 4}))
 }
---
+++ sorting/insertions.go
@@ -0,0 +1,14 @@
+package sorting
+
+func InsertionSort(arr []int) {
+	var n = len(arr)
+	for i := 1; i < n; i++ {
+		j := i
+		for j > 0 {
+			if arr[j-1] > arr[j] {
+				arr[j-1], arr[j] = arr[j], arr[j-1]
+			}
+			j = j - 1
+		}
+	}
+}
