451d9f8f92398807a53ba09808f8dda1d107aa1d
Vladislav Dankevich <vielendanke1991@gmail.com>
2022-02-05 16:24:36 +0600

    algoritms

---
+++ backtracking/all_combinations.go
@@ -0,0 +1,25 @@
+package backtracking
+
+func Combine(n int, k int) [][]int {
+	// 1 to n combinations with k
+	// meaning
+	result := make([][]int, 0)
+	backtrack(&result, make([]int, 0), 1, n, k)
+	return result
+}
+
+func backtrack(result *[][]int, combination []int, start, n, k int) {
+	if len(combination) == k {
+		copiedArr := make([]int, 0)
+		for _, v := range combination {
+			copiedArr = append(copiedArr, v)
+		}
+		*result = append(*result, copiedArr)
+		return
+	}
+	for i := start; i < n+1; i++ {
+		combination = append(combination, i)
+		backtrack(result, combination, start+1, n, k)
+		combination = combination[:len(combination)-1]
+	}
+}
