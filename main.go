d297a2fe58729b0136152c93685b5d12a63e8aec
Vladislav Dankevich <vielendanke1991@gmail.com>
2022-02-08 17:03:17 +0600

    algo

--- main.go
+++ main.go
@@ -3,9 +3,19 @@ package main
 import (
 	"fmt"
 	"github.com/vielendanke/preparation/leetcode/medium"
-	"os"
 )
 
 func main() {
-	fmt.Fprintf(os.Stdout, "Result: %d\n", medium.MaxProduct([]string{"eae", "ea", "aaf", "bda", "fcf", "dc", "ac", "ce", "cefde", "dabae"}))
+	tree := medium.BuildTree([]int{-1}, []int{-1})
+
+	traverseTree(tree)
+}
+
+func traverseTree(node *medium.TreeNode) {
+	if node == nil {
+		return
+	}
+	fmt.Println(node.Val)
+	traverseTree(node.Left)
+	traverseTree(node.Right)
 }
