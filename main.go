2b339589fddfd94b826dcb45bfa2dddaff80d91f
Vladislav Dankevich <vielendanke1991@gmail.com>
2022-02-11 22:39:02 +0600

    algo

--- main.go
+++ main.go
@@ -2,9 +2,10 @@ package main
 
 import (
 	"fmt"
-	"github.com/vielendanke/preparation/leetcode/medium"
+	"github.com/vielendanke/preparation/strings"
 )
 
 func main() {
-	fmt.Printf("Result: %t\n", medium.CheckInclusion("abc", "ccccbbbbaaaa"))
+	fmt.Printf("Result: %s\n", strings.SplitPi("3141592653589793238462643383279",
+		[]string{"314", "49", "9001", "15926535897", "14", "9323", "8462643383279", "4", "793"}))
 }
