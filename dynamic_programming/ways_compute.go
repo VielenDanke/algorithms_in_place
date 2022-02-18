ef66a7d3cc42e390fdbd92f5088cdb4083708569
Vladislav Dankevich <vielendanke1991@gmail.com>
2022-02-18 18:37:28 +0600

    tabulation

---
+++ dynamic_programming/ways_compute.go
@@ -0,0 +1,34 @@
+package main
+
+import "strconv"
+
+func DiffWaysCompute(expression string) []int {
+	result := make([]int, 0)
+	for i := 0; i < len(expression); i++ {
+		c := expression[i]
+		if c == '+' || c == '*' || c == '-' {
+			fSub := expression[0:i]
+			sSub := expression[i+1:]
+			v1 := DiffWaysCompute(fSub)
+			v2 := DiffWaysCompute(sSub)
+			for _, x := range v1 {
+				for _, y := range v2 {
+					if c == '*' {
+						result = append(result, x*y)
+					} else if c == '-' {
+						result = append(result, x-y)
+					} else if c == '+' {
+						result = append(result, x+y)
+					}
+				}
+			}
+		}
+	}
+	if len(result) == 0 {
+		t, err := strconv.Atoi(expression)
+		if err == nil {
+			result = append(result, t)
+		}
+	}
+	return result
+}
---
+++ strings/medium/remove_k_digits.go
@@ -0,0 +1,36 @@
+package medium
+
+func RemoveKdigits(num string, k int) (result string) {
+	if k == 0 {
+		return num
+	}
+	if len(num) <= k {
+		return "0"
+	}
+
+	stack := make([]byte, 0)
+
+	stack = append(stack, num[0])
+
+	for i := 1; i < len(num); i++ {
+		for k > 0 && len(stack) > 0 && num[i] < stack[len(stack)-1] {
+			k--
+			stack = stack[:len(stack)-1]
+		}
+		stack = append(stack, num[i])
+		if len(stack) == 1 && num[i] == '0' {
+			stack = stack[:len(stack)-1]
+		}
+	}
+	for k > 0 && len(stack) > 0 {
+		k--
+		stack = stack[:len(stack)-1]
+	}
+	for _, v := range stack {
+		result += string(v)
+	}
+	if result == "" {
+		return "0"
+	}
+	return
+}
