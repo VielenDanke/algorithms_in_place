b8ee85d38bcfbcd0b536e02d00d6cfe522a4f9a9
Vladislav Dankevich <vielendanke1991@gmail.com>
2022-03-06 11:37:08 +0600

    tabulation

--- javasolutions/MaximumDepthBinaryTree.java
+++ javasolutions/MaximumDepthBinaryTree.java
@@ -1,25 +1,7 @@
 package javasolutions;
 
+import java.util.Objects;
 import java.util.Stack;
-
-class TreeNode {
-    int val;
-    TreeNode left;
-    TreeNode right;
-
-    TreeNode() {
-    }
-
-    TreeNode(int val) {
-        this.val = val;
-    }
-
-    TreeNode(int val, TreeNode left, TreeNode right) {
-        this.val = val;
-        this.left = left;
-        this.right = right;
-    }
-}
 
 public class MaximumDepthBinaryTree {
 
