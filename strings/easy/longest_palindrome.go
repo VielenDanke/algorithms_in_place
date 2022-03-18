1e9bd53f7c1d66e75f166aa15cb8903bdcaf9a73
Vladislav Dankevich <vielendanke1991@gmail.com>
2022-03-19 00:13:39 +0600

    java solutions

---
+++ strings/easy/longest_palindrome.go
@@ -0,0 +1,43 @@
+package easy
+
+func LongestPalindrome2(s string) (result int) {
+	storage := make(map[rune]int)
+	for _, char := range s {
+		storage[char]++
+	}
+	isOneAppend := false
+	for _, char := range s {
+		if v, ok := storage[char]; ok {
+			if (v-1)%2 == 0 && !isOneAppend && v != 1 {
+				result += v
+				isOneAppend = true
+			} else if v%2 == 0 {
+				result += v
+			}
+			delete(storage, char)
+		}
+	}
+	if !isOneAppend && len(storage) != 0 {
+		result += 1
+	}
+	return
+}
+
+func LongestPalindrome(s string) (result int) {
+	m := make(map[rune]int)
+	for _, v := range s {
+		m[v]++
+	}
+	var isOdd bool
+	for _, value := range m {
+		result += value
+		if value%2 == 1 {
+			result--
+			isOdd = true
+		}
+	}
+	if isOdd {
+		result++
+	}
+	return
+}
