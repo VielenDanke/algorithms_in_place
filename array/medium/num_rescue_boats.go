981ecc1dfb99c6cb30a793956954e69f5abe59fe
Vladislav Dankevich <vielendanke1991@gmail.com>
2022-03-24 15:38:46 +0600

    num_rescue_boats.go

---
+++ array/medium/num_rescue_boats.go
@@ -0,0 +1,17 @@
+package medium
+
+import "sort"
+
+func NumRescueBoats(people []int, limit int) (result int) {
+	sort.Ints(people)
+	left, right := 0, len(people)-1
+
+	for left <= right {
+		if people[right]+people[left] <= limit {
+			left++
+		}
+		right--
+		result++
+	}
+	return
+}
