6defb947e4a8b67bce873b08b85d9c0f7b381a14
= <vielendanke1991@gmail.com>
2022-03-26 12:11:54 +0600

    lowest_common_ancestor

---
+++ tree/medium/lowest_common_ancestor.go
@@ -0,0 +1,24 @@
+package medium
+
+import "github.com/vielendanke/preparation/tree"
+
+func lowestCommonAncestor(root, p, q *tree.TreeNode) *tree.TreeNode {
+	if root == nil {
+		return nil
+	}
+	if p.Val == root.Val {
+		return p
+	}
+	if q.Val == root.Val {
+		return q
+	}
+	left := lowestCommonAncestor(root.Left, p, q)
+	right := lowestCommonAncestor(root.Right, p, q)
+	if left != nil && right != nil {
+		return root
+	}
+	if left != nil {
+		return left
+	}
+	return right
+}
