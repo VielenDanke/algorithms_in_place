d5ddcc049639270f58fa3b7b72a48e525fd500f7
Vladislav Dankevich <vielendanke1991@gmail.com>
2022-03-28 17:57:27 +0600

    top_k_frequent

--- main.go
+++ main.go
@@ -1,10 +1,3 @@
 package main
 
-import (
-	"fmt"
-	"github.com/vielendanke/preparation/array/medium"
-)
-
-func main() {
-	fmt.Printf("%v\n", medium.TopKFrequentFaster([]int{3, 2, 3, 1, 2, 4, 5, 5, 6, 7, 7, 8, 2, 3, 1, 1, 1, 10, 11, 5, 6, 2, 4, 7, 8, 5, 6}, 10))
-}
+func main() {}
---
+++ tree/hard/serialize_deserialize_binary_tree_297.go
@@ -0,0 +1,76 @@
+package hard
+
+import (
+	"github.com/vielendanke/preparation/tree"
+	"strconv"
+	"strings"
+)
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+type Codec struct {
+}
+
+func Constructor() Codec {
+	return Codec{}
+}
+
+// Serializes a tree to a single string.
+func (this *Codec) serialize(root *tree.TreeNode) string {
+	data := make([]string, 0)
+	serializeDFS(root, &data)
+	return strings.Join(data, ",")
+}
+
+// Deserializes your encoded data to tree.
+func (this *Codec) deserialize(data string) *tree.TreeNode {
+	treeStr := strings.Split(data, ",")
+	nodes := make([]*tree.TreeNode, len(treeStr))
+
+	for idx := range treeStr {
+		if treeStr[idx] != "nil" {
+			val, _ := strconv.Atoi(treeStr[idx])
+			nodes[idx] = &tree.TreeNode{Val: val}
+		}
+	}
+	return deserializeDFS(&nodes)
+}
+
+func deserializeDFS(nodes *[]*tree.TreeNode) *tree.TreeNode {
+	if len(*nodes) == 0 {
+		return nil
+	}
+	node := (*nodes)[0]
+	*nodes = (*nodes)[1:len(*nodes)]
+	if node == nil {
+		return node
+	}
+	node.Left = deserializeDFS(nodes)
+	node.Right = deserializeDFS(nodes)
+	return node
+}
+
+func serializeDFS(root *tree.TreeNode, data *[]string) {
+	if root == nil {
+		*data = append(*data, "nil")
+		return
+	}
+	*data = append(*data, strconv.Itoa(root.Val))
+	serializeDFS(root.Left, data)
+	serializeDFS(root.Right, data)
+}
+
+/**
+ * Your Codec object will be instantiated and called as such:
+ * ser := Constructor();
+ * deser := Constructor();
+ * data := ser.serialize(root);
+ * ans := deser.deserialize(data);
+ */
