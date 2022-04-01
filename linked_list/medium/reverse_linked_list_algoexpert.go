2fe30a6fe0b80c065d76c015883eb9aac20d4231
Vladislav Dankevich <vielendanke1991@gmail.com>
2022-04-01 13:19:16 +0600

    reverse string

---
+++ linked_list/medium/reverse_linked_list_algoexpert.go
@@ -0,0 +1,33 @@
+package medium
+
+func reverseLinkedList(head *LinkedList) *LinkedList {
+	var previousNode, currentNode *LinkedList = nil, head
+
+	for currentNode != nil {
+		nextNode := currentNode.Next
+		currentNode.Next = previousNode
+		previousNode = currentNode
+		currentNode = nextNode
+	}
+	return previousNode
+}
+
+func reverseLinkedListSecond(head *LinkedList) *LinkedList {
+	array := make([]int, 0)
+
+	temp := head
+
+	for temp != nil {
+		array = append(array, temp.Value)
+		temp = temp.Next
+	}
+	head.Value = array[len(array)-1]
+
+	temp = head
+
+	for i := len(array) - 2; i >= 0; i-- {
+		temp.Next.Value = array[i]
+		temp = temp.Next
+	}
+	return head
+}
