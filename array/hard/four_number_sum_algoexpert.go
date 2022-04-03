f181ad96886ddce6c775f0f1f95b2f14eade2ac9
= <vielendanke1991@gmail.com>
2022-04-03 14:32:23 +0600

    four_number_sum_algoexpert.go

--- array/hard/four_number_sum_algoexpert.go
+++ array/hard/four_number_sum_algoexpert.go
@@ -1,7 +1,30 @@
 package hard
 
+// Average: O(N^2) time | O(N^2) space
+// Worst: O(N^3) time | O(N^2) space
+
 func FourNumberSum(array []int, target int) [][]int {
-	return nil
+	allPairs := make(map[int][][]int)
+	result := make([][]int, 0)
+	for i := 0; i < len(array)-1; i++ {
+		for j := i + 1; j < len(array); j++ {
+			first := array[i]
+			second := array[j]
+			currentSum := first + second
+			difference := target - currentSum
+			if pairs, ok := allPairs[difference]; ok {
+				for _, pair := range pairs {
+					pair = append(pair, first, second)
+					result = append(result, pair)
+				}
+			}
+		}
+		for k := 0; k < i; k++ {
+			currentSum := array[i] + array[k]
+			allPairs[currentSum] = append(allPairs[currentSum], []int{array[k], array[i]})
+		}
+	}
+	return result
 }
 
 func FourNumberSumBruteForce(array []int, target int) [][]int {
--- main.go
+++ main.go
@@ -1,4 +1,10 @@
 package main
 
+import (
+	"fmt"
+	"github.com/vielendanke/preparation/array/hard"
+)
+
 func main() {
+	fmt.Printf("%v\n", hard.FourNumberSum([]int{7, 6, 4, -1, 1, 2}, 16))
 }
