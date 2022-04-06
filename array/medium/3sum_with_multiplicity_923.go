8f1b5a0eeaf0666d234749d4ac288e2d48320d9f
= <vielendanke1991@gmail.com>
2022-04-06 21:18:49 +0600

    crack algo study plan - leetcode

--- array/medium/3sum_with_multiplicity_923.go
+++ array/medium/3sum_with_multiplicity_923.go
@@ -5,31 +5,40 @@ import (
 	"sort"
 )
 
-// TODO: have to finish
-
 func ThreeSumMulti(arr []int, target int) (sum int) {
+	mod := 1_000_000_007
 	sort.Ints(arr)
-
-	repeatCounter := make(map[string]int)
 
 	for i := 0; i < len(arr); i++ {
 		left, right := i+1, len(arr)-1
 		for left < right {
 			tempSum := arr[left] + arr[right] + arr[i]
 
-			if tempSum == target {
-				repeatCounter[createSumKey(arr[i], arr[left], arr[right])]++
-				left++
+			if tempSum > target {
 				right--
 			} else if tempSum < target {
 				left++
-			} else {
+			} else if arr[left] != arr[right] {
+				l, r := 1, 1
+
+				for left+1 < right && arr[left] == arr[left+1] {
+					left++
+					l++
+				}
+				for right-1 > left && arr[right] == arr[right-1] {
+					right--
+					r++
+				}
+				sum += l * r
+				sum %= mod
+				left++
 				right--
+			} else {
+				sum += (right - left + 1) * (right - left) / 2
+				sum %= mod
+				break
 			}
 		}
-	}
-	for _, v := range repeatCounter {
-		sum += v
 	}
 	return
 }
