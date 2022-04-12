a7d5d91f0d2a6ae8886e96fbf38681cce26164b2
= <vielendanke1991@gmail.com>
2022-04-12 15:50:47 +0600

    crack algo study plan - leetcode

--- main.go
+++ main.go
@@ -1,10 +1,4 @@
 package main
 
-import (
-	"fmt"
-	"github.com/vielendanke/algorithms_in_place/array/medium/bfs_dfs"
-)
-
 func main() {
-	fmt.Printf("%v\n", bfs_dfs.OrangesRotting([][]int{{2, 1, 1}, {1, 1, 0}, {0, 1, 1}}))
 }
