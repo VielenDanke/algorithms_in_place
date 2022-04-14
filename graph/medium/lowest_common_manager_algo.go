e283c9e6f906bd0a5e0815aee516065b11bf1bce
= <vielendanke1991@gmail.com>
2022-04-14 22:23:44 +0600

    lowest_common_manager_algo.go

---
+++ graph/medium/lowest_common_manager_algo.go
@@ -0,0 +1,29 @@
+package medium
+
+type OrgChart struct {
+	Name          string
+	DirectReports []*OrgChart
+}
+
+func GetLowestCommonManager(report, first, second *OrgChart) *OrgChart {
+	// Write your code here.
+	if report.Name == first.Name {
+		return report
+	}
+	if report.Name == second.Name {
+		return report
+	}
+	reports := make([]*OrgChart, 0)
+	for _, r := range report.DirectReports {
+		if temp := GetLowestCommonManager(r, first, second); temp != nil {
+			reports = append(reports, temp)
+		}
+	}
+	if len(reports) == 2 {
+		return report
+	}
+	if len(reports) == 1 {
+		return reports[0]
+	}
+	return nil
+}
