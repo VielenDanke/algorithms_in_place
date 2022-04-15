64b399e5b7e46a928152456ed2b791e46afe89fa
= <vielendanke1991@gmail.com>
2022-04-15 22:35:57 +0600

    longest_word_in_dictionary.go

---
+++ strings/medium/longest_word_in_dictionary_720.go
@@ -0,0 +1,27 @@
+package medium
+
+import "strings"
+
+func longestWord(words []string) string {
+	result := ""
+	set := make(map[string]interface{})
+
+	for _, word := range words {
+		set[word] = nil
+	}
+	for _, word := range words {
+		if len(word) > len(result) || len(word) == len(result) && strings.Compare(word, result) < 0 {
+			isGood := true
+			for i := 1; i < len(word); i++ {
+				if _, ok := set[word[0:i]]; !ok {
+					isGood = false
+					break
+				}
+			}
+			if isGood {
+				result = word
+			}
+		}
+	}
+	return result
+}
