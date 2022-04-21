fc6ba24f2f9f7ad7d33edba8af167d73b3f4f24b
Vladislav Dankevich <vielendanke1991@gmail.com>
2022-04-21 17:36:33 +0600

    dynamic

---
+++ dynamic_programming/longest_common_subsequence_1143.go
@@ -0,0 +1,19 @@
+package dynamic_programming
+
+func longestCommonSubsequence(text1 string, text2 string) int {
+	dp := make([][]int, len(text1)+1)
+
+	for i := range dp {
+		dp[i] = make([]int, len(text2))
+	}
+	for i := 0; i < len(text1); i++ {
+		for j := 0; j < len(text2); j++ {
+			if text1[i] == text2[j] {
+				dp[i+1][j+1] = 1 + dp[i][j]
+			} else {
+				dp[i+1][j+1] = max(dp[i][j+1], dp[i+1][j])
+			}
+		}
+	}
+	return dp[len(text1)][len(text2)]
+}
--- main.go
+++ main.go
@@ -1,10 +1,4 @@
 package main
 
-import (
-	"fmt"
-	"github.com/vielendanke/algorithms_in_place/dynamic_programming"
-)
-
 func main() {
-	fmt.Printf("%v\n", dynamic_programming.WordBreak("leetcode", []string{"leet", "code"}))
 }
