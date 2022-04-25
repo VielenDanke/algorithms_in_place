07743c3b43a092eeee8b59d7f311114dd5e45415
Vladislav Dankevich <vielendanke1991@gmail.com>
2022-04-25 12:36:56 +0600

    rooting oranges bfs version added

--- array/medium/golang_solutions/bfs_dfs/rotting_oranges_994.go
+++ array/medium/golang_solutions/bfs_dfs/rotting_oranges_994.go
@@ -53,3 +53,45 @@ func orangesRotting(grid [][]int) (minutes int) {
 	}
 	return
 }
+
+func orangesRottingBFS(grid [][]int) (minutes int) {
+	dir := [][]int{{-1, 0}, {1, 0}, {0, -1}, {0, 1}}
+	rowLength, columnLength := len(grid), len(grid[0])
+	points := make([][]int, 0)
+	goodOranges := 0
+
+	for i := range grid {
+		for j := range grid[i] {
+			if grid[i][j] == 2 {
+				points = append(points, []int{i, j})
+			} else if grid[i][j] == 1 {
+				goodOranges++
+			}
+		}
+	}
+
+	for len(points) > 0 && goodOranges != 0 {
+		currentLength := len(points)
+
+		var currentPoint []int
+
+		for i := 0; i < currentLength; i++ {
+			currentPoint, points = points[0], points[1:]
+			for _, v := range dir {
+				row, column := currentPoint[0], currentPoint[1]
+				nextRow, nextColumn := row+v[0], column+v[1]
+				if nextRow < 0 || nextColumn < 0 || nextRow >= rowLength || nextColumn >= columnLength || grid[nextRow][nextColumn] != 1 {
+					continue
+				}
+				goodOranges--
+				grid[nextRow][nextColumn] = 2
+				points = append(points, []int{nextRow, nextColumn})
+			}
+		}
+		minutes++
+	}
+	if goodOranges == 0 {
+		return minutes
+	}
+	return -1
+}
