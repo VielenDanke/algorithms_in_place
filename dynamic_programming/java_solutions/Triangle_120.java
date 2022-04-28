fa902cdf80b5070646fd41ca0247d37d6914b328
Vladislav Dankevich <vielendanke1991@gmail.com>
2022-04-28 16:39:24 +0600

    word_search_79 java solution

--- dynamic_programming/java_solutions/Triangle_120.java
+++ dynamic_programming/java_solutions/Triangle_120.java
@@ -54,18 +54,16 @@ public class Triangle_120 {
     // ----------------------------------------------------------------------------------------------------------------
 
     public int minimumTotalDP(List<List<Integer>> triangle) {
-        int[][] dp = new int[triangle.size()][triangle.size()];
+        int n = triangle.size();
+        int[] results = new int[n + 1];
 
-        for (int i = 0; i < triangle.size(); i++) {
-            dp[triangle.size() - 1][i] = triangle.get(triangle.size() - 1).get(i);
-        }
+        for (int i = n - 1; i >= 0; i--) {
+            List<Integer> current = triangle.get(i);
 
-        for (int row = triangle.size() - 2; row >= 0; row--) {
-            for (int pos = 0; pos < triangle.get(row).size(); pos++) {
-                dp[row][pos] = triangle.get(row).get(pos) + Math.min(dp[row + 1][pos + 1], dp[row + 1][pos]);
+            for (int j = 0; j < current.size(); j++) {
+                results[j] = Math.min(results[j], results[j + 1]) + current.get(j);
             }
         }
-
-        return dp[0][0];
+        return results[0];
     }
 }
