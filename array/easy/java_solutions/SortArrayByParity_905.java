29221edd4c1e87f86a01174b770546b7fc8ff8a4
Vladislav Dankevich <vielendanke1991@gmail.com>
2022-05-03 00:04:48 +0600

    daily tasks

---
+++ array/easy/java_solutions/SortArrayByParity_905.java
@@ -0,0 +1,52 @@
+package array.easy.java_solutions;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class SortArrayByParity_905 {
+
+    public int[] sortArrayByParity(int[] nums) {
+        return mergeSort(nums);
+    }
+
+    private int[] mergeSort(int[] nums) {
+        if (nums.length <= 1) {
+            return nums;
+        }
+        int middle = nums.length / 2;
+        int[] leftArray = Arrays.copyOfRange(nums, 0, middle);
+        int[] rightArray = Arrays.copyOfRange(nums, middle, nums.length);
+
+        return merge(leftArray, rightArray);
+    }
+
+    private int[] merge(int[] leftArray, int[] rightArray) {
+        int[] result = new int[leftArray.length + rightArray.length];
+
+        int start = 0, end = result.length - 1;
+
+        for (int j : leftArray) {
+            if (j % 2 == 0) {
+                result[start] = j;
+                start++;
+            } else {
+                result[end] = j;
+                end--;
+            }
+        }
+        for (int j : rightArray) {
+            if (j % 2 == 0) {
+                result[start] = j;
+                start++;
+            } else {
+                result[end] = j;
+                end--;
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(new SortArrayByParity_905().sortArrayByParity(new int[]{3,1,2,4})));
+    }
+}
---
+++ array/medium/java_solutions/FindFirstLastPosition_34.java
@@ -0,0 +1,32 @@
+package array.medium.java_solutions;
+
+public class FindFirstLastPosition_34 {
+
+    public int[] searchRange(int[] nums, int target) {
+        if (nums.length == 0) {
+            return new int[]{-1, -1};
+        }
+        int left = 0, right = nums.length - 1;
+
+        while (left <= right) {
+            int middle = (left + right) / 2;
+
+            if (nums[middle] == target) {
+                left = middle - 1;
+                right = middle + 1;
+                while (left >= 0 && nums[middle] == nums[left]) {
+                    left--;
+                }
+                while (right < nums.length && nums[middle] == nums[right]) {
+                    right++;
+                }
+                return new int[]{left + 1, right - 1};
+            } else if (nums[middle] > target) {
+                right = middle - 1;
+            } else {
+                left = middle + 1;
+            }
+        }
+        return new int[]{-1, -1};
+    }
+}
