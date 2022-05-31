314d3ba048e5afe8db1418f5ad892bf316fcf5c2
Vladislav Dankevich <vielendanke1991@gmail.com>
2022-05-31 12:05:54 +0600

    daily tasks + algo course (2 stage)

--- strings/medium/java_solutions/CheckIfStringContainsAllBinaryCodesOfSizeK_1461.java
+++ strings/medium/java_solutions/CheckIfStringContainsAllBinaryCodesOfSizeK_1461.java
@@ -15,6 +15,30 @@ public class CheckIfStringContainsAllBinaryCodesOfSizeK_1461 {
         return binaries.size() == 1 << k;
     }
 
+    // ----------------------------------------------------------------
+    // Binary approach
+    public static boolean hasAllCodesBinary(String s, int k) {
+        int need = 1 << k;
+        boolean[] got = new boolean[need];
+        int allOne = need - 1;
+        int hashVal = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            // calculate hash for s.substr(i-k+1,i+1)
+            hashVal = ((hashVal << 1) & allOne) | (s.charAt(i) - '0');
+            // hash only available when i-k+1 > 0
+            if (i >= k - 1 && !got[hashVal]) {
+                got[hashVal] = true;
+                need--;
+                if (need == 0) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+
     // -----------------------------------------------------------------
     // Backtracking - Time limit
 
