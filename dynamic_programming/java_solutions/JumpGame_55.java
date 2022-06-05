04902102bd6112c508491b8e3156ae33230263b0
Vladislav Dankevich <vielendanke1991@gmail.com>
2022-06-05 15:44:09 +0600

    daily tasks + algo course (2 stage)

--- dynamic_programming/java_solutions/JumpGame_55.java
+++ dynamic_programming/java_solutions/JumpGame_55.java
@@ -6,6 +6,13 @@ public class JumpGame_55 {
     Pattern: start from last and iterate in decreasing order
              try to add to current idx jump length
              if position is reachable - place lastPos to current idx
+
+    Example: [2,3,1,1,4]
+
+             1) idx 3 + 1 >= 4 - yes, set lastPos to 3
+             2) idx 2 + 1 >= 3 - yes, set lastPos to 2
+             3) idx 1 + 3 >= 2 - yes, set lastPos to 1
+             4) idx 0 + 2 >= 1 - yes, set lastPos to 0
      */
 
     public boolean canJump(int[] nums) {
