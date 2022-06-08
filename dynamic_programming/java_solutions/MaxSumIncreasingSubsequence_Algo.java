d59bba547bc298de877ea7df324b84a063bb1743
Vladislav Dankevich <vielendanke1991@gmail.com>
2022-06-08 12:33:53 +0600

    daily tasks + algo course (2 stage)

--- dynamic_programming/java_solutions/MaxSumIncreasingSubsequence_Algo.java
+++ dynamic_programming/java_solutions/MaxSumIncreasingSubsequence_Algo.java
@@ -7,6 +7,15 @@ public class MaxSumIncreasingSubsequence_Algo {
 
     // --------------------------------------------------------------
     // Backtracking
+    /*
+    Pattern: find all increasing subsequences one by one and compare result sum
+    Realisation: backtracking
+    Example:
+    [10, 70, 20, 30, 50, 11, 30]
+
+    1) 10 -> 70 -> skip 20 -> skip 30 -> skip 50 -> skip 11 -> skip 30 = 80 Sum (70 >= all skipped numbers)
+    2) 10 -> 20 -> 30 -> 50 -> skip 11 -> skip 30 = 110 Sum (Notices we enter 10 and recursively returned from 70 and moved to 20)
+     */
 
     public static List<List<Integer>> maxSumIncreasingSubsequence(int[] array) {
         List<List<Integer>> result = new ArrayList<>();
@@ -25,13 +34,13 @@ public class MaxSumIncreasingSubsequence_Algo {
             temp.add(array[i]);
             sum += array[i];
             backtrack(result, temp, array, i + 1, sum);
-            compareAndReplaceSumIfGreater(result, temp, sum);
+            compareAndReplaceIfSumGreater(result, temp, sum);
             sum -= temp.get(temp.size() - 1);
             temp.remove(temp.size() - 1);
         }
     }
 
-    private static void compareAndReplaceSumIfGreater(List<List<Integer>> result, List<Integer> temp, int sum) {
+    private static void compareAndReplaceIfSumGreater(List<List<Integer>> result, List<Integer> temp, int sum) {
         if (result.size() == 0) {
             result.add(new ArrayList<>() {{
                 add(sum);
