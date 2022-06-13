ad51242adb2afb8d0c11e364b36c31a676ec7c0d
Vladislav Dankevich <vielendanke1991@gmail.com>
2022-06-13 12:01:12 +0600

    daily tasks + algo course (2 stage)

---
+++ array/hard/MaxPointsLine_149.java
@@ -0,0 +1,54 @@
+package array.hard;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MaxPointsLine_149 {
+
+    public int maxPoints(int[][] points) {
+        if (points == null) return 0;
+        if (points.length <= 2) return points.length;
+
+        Map<Integer, Map<Integer, Integer>> map = new HashMap<>();
+        int result = 0;
+        for (int i = 0; i < points.length; i++) {
+            map.clear();
+            int overlap = 0, max = 0;
+            for (int j = i + 1; j < points.length; j++) {
+                int x = points[j][0] - points[i][0];
+                int y = points[j][1] - points[i][1];
+                if (x == 0 && y == 0) {
+                    overlap++;
+                    continue;
+                }
+                int gcd = generateGCD(x, y);
+                if (gcd != 0) {
+                    x /= gcd;
+                    y /= gcd;
+                }
+
+                if (map.containsKey(x)) {
+                    if (map.get(x).containsKey(y)) {
+                        map.get(x).put(y, map.get(x).get(y) + 1);
+                    } else {
+                        map.get(x).put(y, 1);
+                    }
+                } else {
+                    Map<Integer, Integer> m = new HashMap<>();
+                    m.put(y, 1);
+                    map.put(x, m);
+                }
+                max = Math.max(max, map.get(x).get(y));
+            }
+            result = Math.max(result, max + overlap + 1);
+        }
+        return result;
+
+
+    }
+
+    private int generateGCD(int a, int b) {
+        if (b == 0) return a;
+        else return generateGCD(b, a % b);
+    }
+}
