337052b073cf87f41008144d263a334e2349acb0
Vladislav Dankevich <vielendanke1991@gmail.com>
2022-06-22 11:17:43 +0600

    daily tasks + algo course (2 stage)

---
+++ array/medium/java_solutions/FindKLargestElement_215.java
@@ -0,0 +1,27 @@
+package array.medium.java_solutions;
+
+import java.util.Arrays;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+public class FindKLargestElement_215 {
+
+    // Sorting O(N * logN) time | O(1) space
+
+    public int findKthLargest(int[] nums, int k) {
+        Arrays.sort(nums);
+        return nums[nums.length - k];
+    }
+
+    // ----------------------------------------------------------------------------
+    // MaxHeap O(K + N * logM) | O(N) space
+    // K - k element from nums, N - length of nums, M - insertion complexity
+
+    public int findKthLargestQueue(int[] nums, int k) {
+        Queue<Integer> queue = new PriorityQueue<>((l, r) -> r - l);
+        for (int num : nums) queue.add(num);
+        int lastElem = 0;
+        while (k-- != 0 && !queue.isEmpty()) lastElem = queue.poll();
+        return lastElem;
+    }
+}
