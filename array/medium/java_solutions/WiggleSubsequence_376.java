c0192e0524a181416f444ad46019a901cfa4ea65
Vladislav Dankevich <vielendanke1991@gmail.com>
2022-07-05 11:06:29 +0600

    daily tasks + algo course (2 stage)

--- array/medium/java_solutions/WiggleSubsequence_376.java
+++ array/medium/java_solutions/WiggleSubsequence_376.java
@@ -1,10 +1,35 @@
 package array.medium.java_solutions;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
 public class WiggleSubsequence_376 {
 
+    // DP
+    public int wiggleMaxLengthDP(int[] nums) {
+        if (nums.length == 0) return 0;
+        int[] up = new int[nums.length];
+        int[] down = new int[nums.length];
+        up[0] = 1;
+        down[0] = 1;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] > nums[i - 1]) {
+                up[i] = down[i - 1] + 1;
+                down[i] = down[i - 1];
+            } else if (nums[i] < nums[i - 1]) {
+                down[i] = up[i - 1] + 1;
+                up[i] = up[i - 1];
+            } else {
+                down[i] = down[i - 1];
+                up[i] = up[i - 1];
+            }
+        }
+        return Math.max(down[nums.length - 1], up[nums.length - 1]);
+    }
+
+    // --------------------------------------------------------------------------------------------------------------
+    // memo
     private final Map<String, Integer> memo = new HashMap<>();
 
     public int wiggleMaxLength(int[] nums) {
