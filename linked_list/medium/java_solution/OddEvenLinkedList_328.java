c9322cab5af91b6c17b95c626ae1b36d5c9b5861
Vladislav Dankevich <vielendanke1991@gmail.com>
2022-07-29 22:52:38 +0600

    daily tasks + algo course (2 stage)

--- linked_list/medium/java_solution/OddEvenLinkedList_328.java
+++ linked_list/medium/java_solution/OddEvenLinkedList_328.java
@@ -63,9 +63,5 @@ public class OddEvenLinkedList_328 {
             dummy.next = null;
             return result.next;
         }
-
-        public static void main(String[] args) {
-            new Solution().oddEvenList(new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5))))));
-        }
     }
 }
