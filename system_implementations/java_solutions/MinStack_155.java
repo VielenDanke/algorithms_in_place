ef1eab901141cdeff3e2a35edd7d24690f50d79c
Vladislav Dankevich <vielendanke1991@gmail.com>
2022-09-02 22:44:53 +0600

    daily task

---
+++ system_implementations/java_solutions/MinStack_155.java
@@ -0,0 +1,45 @@
+package system_implementations.java_solutions;
+
+import java.util.Stack;
+
+public class MinStack_155 {
+
+    private static class MinStack {
+
+        private final Stack<Integer> stack;
+        private Integer currentMin;
+
+        public MinStack() {
+            this.stack = new Stack<>();
+        }
+
+        public void push(int val) {
+            stack.add(val);
+            replaceIfBigger(val);
+        }
+
+        public void pop() {
+            int current = stack.pop();
+            if (current == currentMin) {
+                currentMin = null;
+                for (Integer num : stack) {
+                    replaceIfBigger(num);
+                }
+            }
+        }
+
+        public int top() {
+            return stack.peek();
+        }
+
+        public int getMin() {
+            return currentMin;
+        }
+
+        private void replaceIfBigger(int val) {
+            if (currentMin == null || currentMin > val) {
+                currentMin = val;
+            }
+        }
+    }
+}
