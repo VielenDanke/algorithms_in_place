0207b0e44e1e67fef847c8c3202239e8a66ac6f0
Vladislav Dankevich <vielendanke1991@gmail.com>
2022-09-14 09:43:43 +0600

    daily task

---
+++ array/medium/java_solutions/Utf8Validation_393.java
@@ -0,0 +1,52 @@
+package array.medium.java_solutions;
+
+public class Utf8Validation_393 {
+
+    static class Solution {
+        public boolean validUtf8(int[] data) {
+            int bytesRemaining = 0;
+            for (int d : data) {
+                if (bytesRemaining == 0) {
+                    if ((d >> 5) == 0b110) bytesRemaining = 1;
+                    else if ((d >> 4) == 0b1110) bytesRemaining = 2;
+                    else if ((d >> 3) == 0b11110) bytesRemaining = 3;
+                    else if ((d >> 7) != 0) return false;
+                } else {
+                    if ((d >> 6) != 0b10) return false;
+                    bytesRemaining--;
+                }
+            }
+            return bytesRemaining == 0;
+        }
+    }
+
+    static class SolutionWithBits {
+        public boolean validUtf8(int[] data) {
+            int count = 0;
+
+            for (int current : data) {
+                if (current >= 128 && current <= 191) {
+                    if (count-- == 0) {
+                        return false;
+                    }
+                } else {
+                    if (count > 0) {
+                        return false;
+                    }
+                    if (current < 128) {
+                        continue;
+                    } else if (current < 224) {
+                        count = 1;
+                    } else if (current < 240) {
+                        count = 2;
+                    } else if (current < 248) {
+                        count = 3;
+                    } else {
+                        return false;
+                    }
+                }
+            }
+            return count == 0;
+        }
+    }
+}
---
+++ graph/medium/java_solutions/GasStation_134.java
@@ -0,0 +1,92 @@
+package graph.medium.java_solutions;
+
+public class GasStation_134 {
+
+    static class Solution {
+        public int canCompleteCircuit(int[] gas, int[] cost) {
+            int n = gas.length;
+            int total = 0;
+            int surplus = 0;
+            int start = 0;
+
+            for(int i = 0; i < n; i++){
+                total += gas[i] - cost[i];
+                surplus += gas[i] - cost[i];
+                if(surplus < 0){
+                    surplus = 0;
+                    start = i + 1;
+                }
+            }
+            return (total < 0) ? -1 : start;
+        }
+    }
+
+    static class SolutionBruteForce {
+
+        public int canCompleteCircuit(int[] gas, int[] cost) {
+            int len = gas.length;
+            for (int i = 0; i < len; i++) {
+                int stopCount = 0, j = i, totalGas = 0;
+                while (stopCount < len) {
+                    totalGas += gas[j % len] - cost[j % len];
+                    if (totalGas < 0) break;
+                    stopCount++;
+                    j++;
+                }
+                if (stopCount == len) return i;
+            }
+            return -1;
+        }
+    }
+
+    static class SolutionGraph {
+
+        private static class Node {
+            int gas;
+            int cost;
+            int idx;
+            Node next;
+
+            Node(int gas, int cost, int idx) {
+                this.gas = gas;
+                this.cost = cost;
+                this.idx = idx;
+            }
+        }
+
+        public int canCompleteCircuit(int[] gas, int[] cost) {
+            Node node = new Node(gas[0], cost[0], 0);
+            Node head = node;
+
+            for (int i = 1; i < gas.length; i++) {
+                node.next = new Node(gas[i], cost[i], i);
+                node = node.next;
+            }
+            node.next = head;
+            Node start = head;
+            while (head != null) {
+                boolean[] visited = new boolean[gas.length];
+                if (find(head, head.gas, visited)) {
+                    return head.idx;
+                }
+                head=head.next;
+                if (head == start) {
+                    break;
+                }
+            }
+            return -1;
+        }
+
+        private boolean find(Node node, int gasAmount, boolean[] visited) {
+            if (visited[node.idx]) {
+                return true;
+            }
+            if (gasAmount < node.cost) {
+                return false;
+            }
+            visited[node.idx] = true;
+            gasAmount -= node.cost;
+            return find(node.next, gasAmount + node.next.gas, visited);
+        }
+    }
+}
