4cd9a3630787bae3d4b946ccd0c3ef06bdbde784
Vladislav Dankevich <vielendanke1991@gmail.com>
2022-09-29 11:33:49 +0600

    daily task

--- array/medium/java_solutions/FindKClosestElements_658.java
+++ array/medium/java_solutions/FindKClosestElements_658.java
@@ -4,6 +4,23 @@ import java.util.*;
 
 public class FindKClosestElements_658 {
 
+    static class SolutionTwoPointers {
+        public List<Integer> findClosestElements(int[] arr, int k, int x) {
+            int left = 0, right = arr.length - 1;
+
+            while (right - left >= k) {
+                if (Math.abs(arr[left] - x) > Math.abs(arr[right] - x)) {
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+            List<Integer> result = new ArrayList<>();
+            for (int i = left; i <= right; i++) result.add(arr[i]);
+            return result;
+        }
+    }
+
     static class Solution {
         /*
         arr = [1,2,3,4,5]
