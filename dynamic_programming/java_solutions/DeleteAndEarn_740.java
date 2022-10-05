f77a92d3894f459b2e0697bd825354f3d46b16f0
Vladislav Dankevich <vielendanke1991@gmail.com>
2022-10-05 10:08:22 +0600

    daily task

--- dynamic_programming/java_solutions/DeleteAndEarn_740.java
+++ dynamic_programming/java_solutions/DeleteAndEarn_740.java
@@ -2,30 +2,6 @@ package dynamic_programming.java_solutions;
 
 public class DeleteAndEarn_740 {
 
-    static class SolutionRecursive {
-
-        private static final int MAX = 10000;
-
-        public int deleteAndEarn(int[] nums) {
-            int[] count = new int[MAX + 1];
-            int maxNum = 0;
-
-            for (int num: nums) {
-                count[num]++;
-                maxNum = Math.max(maxNum, num);
-            }
-
-            return deleteAndEarn(maxNum, count);
-        }
-
-        private int deleteAndEarn(int num, int[] count) {
-            if (num <= 0) return 0;
-
-            return Math.max(deleteAndEarn(num - 2, count) + (count[num] * num),
-                    deleteAndEarn(num - 1, count));
-        }
-    }
-
     static class SolutionMemo {
         private static final int MAX = 10000;
 
@@ -50,44 +26,19 @@ public class DeleteAndEarn_740 {
         }
     }
 
-    static class SolutionBottomUp {
-        private static final int MAX = 10000;
-
-        public int deleteAndEarn(int[] nums) {
-            int[] count = new int[MAX + 1];
-            int maxNum = 0;
-
-            for (int num: nums) {
-                count[num]++;
-                maxNum = Math.max(maxNum, num);
-            }
-            return deleteAndEarn(maxNum, count);
-        }
-
-        private int deleteAndEarn(int maxNum, int[] count) {
-            int[] maxPoints = new int[MAX + 1];
-
-            for (int num = 1; num <= maxNum; num++) {
-                maxPoints[num] = Math.max(maxPoints[num == 1 ? 0 : num - 2] + (count[num] * num),
-                        maxPoints[num - 1]);
-            }
-            return maxPoints[maxNum];
-        }
-    }
-
     static class Solution {
 
         public int deleteAndEarn(int[] nums) {
-            int[] sum = new int[10001];
+            int[] sum = new int[10002];
+
             for (int num : nums) {
                 sum[num] += num;
             }
-            int[] dp = new int[10001];
-            dp[1] = sum[1];
-            for (int i = 2; i <= 10000; i++) {
-                dp[i] = Math.max(dp[i - 1], dp[i - 2] + sum[i]);
+
+            for(int i = 2; i < sum.length; i++){
+                sum[i] = Math.max(sum[i-1], sum[i-2] + sum[i]);
             }
-            return dp[10000];
+            return sum[10001];
         }
     }
 }
