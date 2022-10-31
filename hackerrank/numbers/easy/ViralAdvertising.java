f5f2b24fe109d89842af5e9a6c142b1403dedc8d
Vladislav Dankevich <vielendanke1991@gmail.com>
2022-10-31 11:13:12 +0600

    daily task

---
+++ hackerrank/numbers/easy/ViralAdvertising.java
@@ -0,0 +1,16 @@
+package hackerrank.numbers.easy;
+
+public class ViralAdvertising {
+
+    // https://www.hackerrank.com/challenges/strange-advertising
+
+    public static int viralAdvertising(int n) {
+        int result = 0;
+        for (int people = 5, liked, i = 0; i < n; i++) {
+            liked = people / 2;
+            people = liked * 3;
+            result += liked;
+        }
+        return result;
+    }
+}
