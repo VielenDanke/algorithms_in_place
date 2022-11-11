38036cebe9d7cc4c48bf64b9a42b6242c86d90d8
Vladislav Dankevich <vielendanke1991@gmail.com>
2022-11-11 22:17:24 +0600

    daily task

---
+++ hackerrank/string/easy/AppendAndDelete.java
@@ -0,0 +1,28 @@
+package hackerrank.string.easy;
+
+public class AppendAndDelete {
+
+    // https://www.hackerrank.com/challenges/append-and-delete/problem
+
+    static class Solution {
+        public static String appendAndDelete(String s, String t, int k) {
+            int n = s.length(), m = t.length();
+            if (n + m <= k) return "Yes";
+            int idx = 0;
+            while (idx < Math.min(n, m) && s.charAt(idx) == t.charAt(idx)) {
+                idx++;
+            }
+            int res = (n - idx) + (m - idx);
+            if (res < k) {
+                while (res < k) {
+                    res += 2;
+                }
+                return res == k ? "Yes" : "No";
+            } else if (res == k) {
+                return "Yes";
+            } else {
+                return "No";
+            }
+        }
+    }
+}
