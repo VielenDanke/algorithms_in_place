34c60c9e38c2f2ed90e84557078c77dce0e466f1
Vladislav Dankevich <vielendanke1991@gmail.com>
2022-12-19 09:50:01 +0600

    daily task

---
+++ leetcode/graph/easy/java_solutions/FindIfPathExistsInGraph_1971.java
@@ -0,0 +1,71 @@
+package leetcode.graph.easy.java_solutions;
+
+import java.util.*;
+
+public class FindIfPathExistsInGraph_1971 {
+
+    static class SolutionUnionFind {
+        private int[] id;
+
+        public boolean validPath(int n, int[][] edges, int source, int destination) {
+            id = new int[n];
+
+            for (int i = 0; i < n; i++) {
+                id[i] = i;
+            }
+            for (int[] edge : edges) {
+                union(edge[0], edge[1]);
+            }
+            return find(source) == find(destination);
+        }
+
+        private void union(int p, int q) {
+            id[find(p)] = find(q);
+        }
+
+        private int find(int p) {
+            while (p != id[p]) {
+                id[p] = id[id[p]];
+                p = id[p];
+            }
+            return p;
+        }
+    }
+
+    static class Solution {
+        public boolean validPath(int n, int[][] edges, int source, int destination) {
+            Map<Integer, List<Integer>> graph = buildGraph(edges);
+            return dfs(graph, source, destination, new HashSet<>());
+        }
+
+        private boolean dfs(Map<Integer, List<Integer>> graph, int position, int endDestination, Set<Integer> visited) {
+            if (position == endDestination) {
+                return true;
+            }
+            if (visited.contains(position)) {
+                return false;
+            }
+            List<Integer> allDest = graph.get(position);
+
+            visited.add(position);
+
+            for (Integer dest : allDest) {
+                if (dfs(graph, dest, endDestination, visited)) {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        private Map<Integer, List<Integer>> buildGraph(int[][] edges) {
+            Map<Integer, List<Integer>> m = new HashMap<>();
+            for (int[] current : edges) {
+                m.putIfAbsent(current[0], new ArrayList<>());
+                m.putIfAbsent(current[1], new ArrayList<>());
+                m.get(current[0]).add(current[1]);
+                m.get(current[1]).add(current[0]);
+            }
+            return m;
+        }
+    }
+}
