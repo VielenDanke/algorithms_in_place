72c53c77d1309412d02788cac7c0cad20e2d4c3a
Vladislav Dankevich <vielendanke1991@gmail.com>
2022-12-21 11:14:16 +0600

    daily task

--- hackerrank/array/easy/golang_solutions/manasa_and_stones.go
+++ hackerrank/array/easy/golang_solutions/manasa_and_stones.go
@@ -3,23 +3,12 @@ package golang_solutions
 import "sort"
 
 func stones(n, a, b int32) (result []int32) {
-	result = append(result, a*n)
-	result = append(result, b*n)
-
-	visited := make(map[int32]interface{})
-
-	for i := int32(1); i <= n/2; i++ {
-		aResult := (a * i) + (b * (n - i))
-		bResult := (b * i) + (a * (n - i))
-
-		if _, ok := visited[aResult]; !ok {
-			visited[aResult] = nil
-			result = append(result, aResult)
-		}
-		if _, ok := visited[bResult]; !ok {
-			visited[bResult] = nil
-			result = append(result, bResult)
-		}
+	set := make(map[int32]interface{})
+	for i := int32(0); i < n; i++ {
+		set[(i*a)+(n-i-1)*b] = nil
+	}
+	for k := range set {
+		result = append(result, k)
 	}
 	sort.Slice(result, func(i, j int) bool {
 		return result[i] < result[j]
@@ -29,8 +18,7 @@ func stones(n, a, b int32) (result []int32) {
 
 // ----------------------------------------------------------------------
 
-func stonesBacktrack(n int32, a int32, b int32) (result []int32) {
-	// Write your code here
+func stonesBacktrack(n, a, b int32) (result []int32) {
 	permutations := make([][]int32, 0)
 
 	backtrack(&permutations, make([]int32, 0), n, a, b)
@@ -38,7 +26,11 @@ func stonesBacktrack(n int32, a int32, b int32) (result []int32) {
 	set := make(map[int32]interface{})
 
 	for _, perm := range permutations {
-		temp := perm[0] + perm[1]
+		temp := int32(0)
+
+		for _, v := range perm {
+			temp += v
+		}
 		if _, ok := set[temp]; !ok {
 			result = append(result, temp)
 			set[temp] = nil
@@ -48,13 +40,10 @@ func stonesBacktrack(n int32, a int32, b int32) (result []int32) {
 }
 
 func backtrack(permutations *[][]int32, temp []int32, n, a, b int32) {
-	if int32(len(temp)) == n {
+	if int32(len(temp)) == n-1 {
 		*permutations = append(*permutations, append(make([]int32, 0), temp...))
 		return
 	}
-	for i := a; i <= b; i++ {
-		temp = append(temp, i)
-		backtrack(permutations, temp, n, a, b)
-		temp = temp[:len(temp)-1]
-	}
+	backtrack(permutations, append(temp, a), n, a, b)
+	backtrack(permutations, append(temp, b), n, a, b)
 }
