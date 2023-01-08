caf6a65867dfa5cfeee8ce2cc07da91c4801b55d
Vladislav Dankevich <vielendanke1991@gmail.com>
2023-01-08 13:40:41 +0600

    daily task

--- leetcode/geometry/hard/MaxPointsLine_149.java
+++ leetcode/geometry/hard/MaxPointsLine_149.java
@@ -5,6 +5,43 @@ import java.util.Map;
 
 public class MaxPointsLine_149 {
 
+    static class SolutionDiff {
+        public int maxPoints(int[][] points) {
+            int maxPoints = 0;
+            for (int i = 0; i < points.length; i++) {
+                Map<String, Integer> slopes = new HashMap<>();
+                int samePoints = 1;
+                for (int j = i + 1; j < points.length; j++) {
+                    int x1 = points[i][0];
+                    int y1 = points[i][1];
+                    int x2 = points[j][0];
+                    int y2 = points[j][1];
+                    if (x1 == x2 && y1 == y2) {
+                        samePoints++;
+                    } else {
+                        String slope;
+                        if (x1 == x2) {
+                            slope = "inf";
+                        } else {
+                            int dy = y2 - y1;
+                            int dx = x2 - x1;
+                            int gcd = gcd(dy, dx);
+                            dy /= gcd;
+                            dx /= gcd;
+                            slope = dy + "/" + dx;
+                        }
+                        slopes.put(slope, slopes.getOrDefault(slope, 0) + 1);
+                    }
+                }
+                maxPoints = Math.max(maxPoints, samePoints);
+                for (int numPoints : slopes.values()) {
+                    maxPoints = Math.max(maxPoints, numPoints + samePoints);
+                }
+            }
+            return maxPoints;
+        }
+    }
+
     static class Solution {
         public int maxPoints(int[][] points) {
             if (points == null) return 0;
@@ -22,7 +59,7 @@ public class MaxPointsLine_149 {
                         overlap++;
                         continue;
                     }
-                    int gcd = getGcd(x, y);
+                    int gcd = gcd(x, y);
                     if (gcd != 0) {
                         x /= gcd;
                         y /= gcd;
@@ -44,10 +81,12 @@ public class MaxPointsLine_149 {
             }
             return result;
         }
+    }
 
-        private int getGcd(int a, int b) {
-            if (b == 0) return a;
-            return getGcd(b, a % b);
+    private static int gcd(int a, int b) {
+        if (b == 0) {
+            return a;
         }
+        return gcd(b, a % b);
     }
 }
