19165b0cd6e478ed16e26a21d0893ac8b3bef46e
Vladislav Dankevich <vielendanke1991@gmail.com>
2023-01-10 14:05:58 +0600

    daily task

--- leetcode/tree/easy/java_solutions/IsSameTree_100.java
+++ leetcode/tree/easy/java_solutions/IsSameTree_100.java
@@ -6,16 +6,10 @@ public class IsSameTree_100 {
 
     private static class Solution {
         public boolean isSameTree(TreeNode p, TreeNode q) {
-            if (p == null && q == null) {
-                return true;
-            } else if (p == null || q == null) {
-                return false;
-            } else {
-                if (p.val != q.val) {
-                    return false;
-                }
-                return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
-            }
+            if (p == null && q == null) return true;
+            if (p == null || q == null) return false;
+            if (p.val != q.val) return false;
+            return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
         }
     }
 }
