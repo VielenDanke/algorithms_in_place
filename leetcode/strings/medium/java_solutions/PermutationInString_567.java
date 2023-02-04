46d56a913ae4a47debdedf4e9351a9127f3c1ed3
Vladislav Dankevich <vielendanke1991@gmail.com>
2023-02-04 11:10:31 +0600

    daily task

---
+++ leetcode/strings/medium/java_solutions/PermutationInString_567.java
@@ -0,0 +1,41 @@
+package leetcode.strings.medium.java_solutions;
+
+public class PermutationInString_567 {
+
+    static class Solution {
+
+        private static final int ALPH_NUMBER = 26;
+
+        public boolean checkInclusion(String s1, String s2) {
+            int n = s1.length();
+            int m = s2.length();
+
+            if (n > m) return false;
+
+            int[] alph = new int[ALPH_NUMBER];
+            int[] sub = new int[ALPH_NUMBER];
+
+            for (char c : s1.toCharArray()) {
+                sub[c - 'a']++;
+            }
+            for (int i = 0; i < n; i++) {
+                alph[s2.charAt(i) - 'a']++;
+            }
+            if (isPermutationsEqual(sub, alph)) return true;
+
+            for (int i = n; i < m; i++) {
+                alph[s2.charAt(i - n) - 'a']--;
+                alph[s2.charAt(i) - 'a']++;
+                if (isPermutationsEqual(sub, alph)) return true;
+            }
+            return false;
+        }
+
+        private boolean isPermutationsEqual(int[] left, int[] right) {
+            for (int i = 0; i < ALPH_NUMBER; i++) {
+                if (left[i] != right[i]) return false;
+            }
+            return true;
+        }
+    }
+}
