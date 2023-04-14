78f5ac4f55d36af55a95b5cad4a350d2535f2963
Vladislav Dankevich <vielendanke1991@gmail.com>
2023-04-14 15:53:24 +0600

    daily task

--- leetcode/dynamic_programming/java_solutions/LongestPalindromicSubsequence_516.java
+++ leetcode/dynamic_programming/java_solutions/LongestPalindromicSubsequence_516.java
@@ -2,7 +2,7 @@ package leetcode.dynamic_programming.java_solutions;
 
 public class LongestPalindromicSubsequence_516 {
 
-    static class Solution {
+    static class SolutionRecursion {
         private Integer[][] cache;
 
         public int longestPalindromeSubseq(String s) {
@@ -16,6 +16,25 @@ public class LongestPalindromicSubsequence_516 {
             if (cache[i][j] != null) return cache[i][j];
             if (s.charAt(i) == s.charAt(j)) return cache[i][j] = dp(s, i + 1, j - 1) + 2;
             return cache[i][j] = Math.max(dp(s, i + 1, j), dp(s, i, j - 1));
+        }
+    }
+
+    static class Solution {
+        public int longestPalindromeSubseq(String s) {
+            int n = s.length();
+            int[][] dp = new int[n][n];
+
+            for (int i = n - 1; i >= 0; i--) {
+                dp[i][i] = 1;
+                for (int j = i + 1; j < n; j++) {
+                    if (s.charAt(i) == s.charAt(j)) {
+                        dp[i][j] = dp[i + 1][j - 1] + 2;
+                    } else {
+                        dp[i][j] = Math.max(dp[i+1][j], dp[i][j-1]);
+                    }
+                }
+            }
+            return dp[0][n - 1];
         }
     }
 }
