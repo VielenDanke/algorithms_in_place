0b222c7c8f0efe402c92d0d396f7953c06bfb19f
Vladislav Dankevich <vielendanke1991@gmail.com>
2023-04-23 14:20:00 +0600

    daily task

---
+++ leetcode/dynamic_programming/java_solutions/RestoreTheArray_1416.java
@@ -0,0 +1,32 @@
+package leetcode.dynamic_programming.java_solutions;
+
+public class RestoreTheArray_1416 {
+
+    static class Solution {
+        private static final long MOD = 1000000007;
+
+        private int k;
+        private Integer[] cache;
+
+        public int numberOfArrays(String s, int k) {
+            this.k = k;
+            this.cache = new Integer[s.length()];
+            return backtrack(s, 0);
+        }
+
+        private int backtrack(String s, int start) {
+            if (start >= s.length()) return 1;
+            if (s.charAt(start) == '0') return 0;
+            if (cache[start] != null) return cache[start];
+            long num = 0;
+            int answer = 0;
+            for (int i = start; i < s.length(); i++) {
+                num = num * 10 + s.charAt(i) - '0';
+                if (num > k) break;
+                answer += backtrack(s, i + 1);
+                answer %= MOD;
+            }
+            return cache[start] = answer;
+        }
+    }
+}
