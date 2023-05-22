7497ae5d782bdbc053a9b3a0dcf45a609f8bac83
Vladislav Dankevich <vielendanke1991@gmail.com>
2023-05-22 08:13:35 +0600

    daily task

--- leetcode/array/medium/java_solutions/TopKFrequentElements_347.java
+++ leetcode/array/medium/java_solutions/TopKFrequentElements_347.java
@@ -1,9 +1,6 @@
 package leetcode.array.medium.java_solutions;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.PriorityQueue;
-import java.util.Queue;
+import java.util.*;
 
 public class TopKFrequentElements_347 {
 
@@ -28,6 +25,36 @@ public class TopKFrequentElements_347 {
 
             while (k-- > 0 && !queue.isEmpty()) {
                 result[counter++] = queue.poll().num;
+            }
+            return result;
+        }
+    }
+
+    static class SolutionTreeMap {
+
+        public int[] topKFrequent(int[] nums, int k) {
+            Map<Integer, Integer> m = new HashMap<>();
+            TreeMap<Integer, List<Integer>> treeMap = new TreeMap<>((l, r) -> r - l);
+
+            for (int n : nums) {
+                m.put(n, m.getOrDefault(n, 0) + 1);
+            }
+
+            for (Map.Entry<Integer, Integer> entry : m.entrySet()) {
+                treeMap.putIfAbsent(entry.getValue(), new LinkedList<>());
+                treeMap.get(entry.getValue()).add(entry.getKey());
+            }
+            int[] result = new int[k];
+            int counter = 0;
+
+            endOfIteration:
+            while (!treeMap.isEmpty()) {
+                for (Integer n : treeMap.pollFirstEntry().getValue()) {
+                    result[counter++] = n;
+                    if (--k <= 0) {
+                        break endOfIteration;
+                    }
+                }
             }
             return result;
         }
