129defdc19d2a88decdf0bac5a19a9f06d942766
Vladislav Dankevich <vielendanke1991@gmail.com>
2023-08-14 09:55:02 +0600

    daily task

--- hackerrank/string/medium/golang_solutions/sherlock_and_the_valid_string.go
+++ hackerrank/string/medium/golang_solutions/sherlock_and_the_valid_string.go
@@ -17,20 +17,30 @@ func isValid(s string) string {
 		min = int(math.Min(float64(min), float64(v)))
 		max = int(math.Max(float64(max), float64(v)))
 	}
-	if math.Abs(float64(max-min)) > 1 {
-		return "NO"
-	}
 	if max == min {
 		return "YES"
 	}
+	minCounter := 0
 	diffCounter := 0
+	maxCounter := 0
 	for _, v := range alph {
 		if v == 0 {
 			continue
 		}
-		if min != v {
+		if min == v {
+			minCounter++
+		} else {
 			diffCounter++
 		}
+		if max == v {
+			maxCounter++
+		}
+	}
+	if minCounter == 1 && (maxCounter*max)+(minCounter*min) == len(s) {
+		return "YES"
+	}
+	if math.Abs(float64(max-min)) > 1 {
+		return "NO"
 	}
 	if diffCounter <= 1 {
 		return "YES"
