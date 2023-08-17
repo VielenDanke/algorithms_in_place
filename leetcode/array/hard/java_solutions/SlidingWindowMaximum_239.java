0eda2759c0a032b9f6cf3e4907cc72f8f16a1267
Vladislav Dankevich <vielendanke1991@gmail.com>
2023-08-17 10:22:36 +0600

    daily task

--- leetcode/array/hard/java_solutions/SlidingWindowMaximum_239.java
+++ leetcode/array/hard/java_solutions/SlidingWindowMaximum_239.java
@@ -1,12 +1,39 @@
 package leetcode.array.hard.java_solutions;
 
 import java.util.Arrays;
+import java.util.Map;
 import java.util.PriorityQueue;
-import java.util.TreeSet;
+import java.util.TreeMap;
 
 public class SlidingWindowMaximum_239 {
 
-    static class Solution {
+    static class SolutionTreeMap {
+        public int[] maxSlidingWindow(int[] nums, int k) {
+            TreeMap<Integer, Integer> map = new TreeMap<>((left, right) -> right - left);
+
+            int size = 0, currentIdx = 0;
+
+            for (int i = 0; i < nums.length; i++) {
+                int currentNum = nums[i];
+                map.put(currentNum, map.getOrDefault(currentNum, 0) + 1);
+                size++;
+                if (size == k) {
+                    Map.Entry<Integer, Integer> entry = map.firstEntry();
+                    int max = entry.getKey();
+                    int prevNum = nums[i - k + 1];
+                    map.put(prevNum, map.getOrDefault(prevNum, 0) - 1);
+                    if (map.get(prevNum) <= 0) {
+                        map.remove(prevNum);
+                    }
+                    nums[currentIdx++] = max;
+                    size--;
+                }
+            }
+            return Arrays.copyOfRange(nums, 0, currentIdx);
+        }
+    }
+
+    static class SolutionPriorityQueue {
         public int[] maxSlidingWindow(int[] nums, int k) {
             PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> nums[b] - nums[a]);
             int n = nums.length;
