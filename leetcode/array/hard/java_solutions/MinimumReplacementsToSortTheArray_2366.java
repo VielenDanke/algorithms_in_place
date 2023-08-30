d1eed5cd407d6d0ab0c33ac8d8edf6d254311bbb
Vladislav Dankevich <vielendanke1991@gmail.com>
2023-08-30 10:31:08 +0600

    daily task

---
+++ leetcode/array/hard/java_solutions/MinimumReplacementsToSortTheArray_2366.java
@@ -0,0 +1,19 @@
+package leetcode.array.hard.java_solutions;
+
+public class MinimumReplacementsToSortTheArray_2366 {
+
+    static class Solution {
+        public long minimumReplacement(int[] nums) {
+            var operations = 0L;
+            var prevBound = nums[nums.length - 1];
+
+            for (int i = nums.length - 2; i >= 0; i--) {
+                var num = nums[i];
+                var noOfTimes = (num + prevBound - 1) / prevBound;
+                operations += noOfTimes - 1;
+                prevBound = num / noOfTimes;
+            }
+            return operations;
+        }
+    }
+}
