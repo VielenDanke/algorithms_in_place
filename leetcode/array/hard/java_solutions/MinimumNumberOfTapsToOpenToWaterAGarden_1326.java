f2ccd01efc20321f24d4714f4cbbaf69dce04299
Vladislav Dankevich <vielendanke1991@gmail.com>
2023-08-31 10:41:22 +0600

    daily task

--- leetcode/array/hard/java_solutions/MinimumNumberOfTapsToOpenToWaterAGarden_1326.java
+++ leetcode/array/hard/java_solutions/MinimumNumberOfTapsToOpenToWaterAGarden_1326.java
@@ -8,8 +8,6 @@ public class MinimumNumberOfTapsToOpenToWaterAGarden_1326 {
 
     static class Solution {
 
-        private int minCounter = Integer.MAX_VALUE;
-
         public int minTaps(int n, int[] ranges) {
             int[][] rangesExpanded = new int[n + 1][2];
 
@@ -20,27 +18,37 @@ public class MinimumNumberOfTapsToOpenToWaterAGarden_1326 {
 
             System.out.println(Arrays.deepToString(rangesExpanded));
 
-            for (int i = 0; i <= n; i++) {
+            Integer minCounter = null;
+
+            for (int i = 0; i < n + 1; i++) {
                 LinkedList<int[]> l = new LinkedList<>();
                 l.add(rangesExpanded[i]);
-                backtrack(l, rangesExpanded, n, i + 1, 1);
+                Integer counter = backtrack(l, rangesExpanded, n, i + 1);
+                if (counter != null) {
+                    minCounter = Math.min(counter, minCounter == null ? Integer.MAX_VALUE : minCounter);
+                }
             }
-            return minCounter == Integer.MAX_VALUE ? -1 : minCounter;
+            return minCounter == null ? -1 : minCounter;
         }
 
-        private void backtrack(LinkedList<int[]> l, int[][] ranges, int n, int j, int counter) {
+        private Integer backtrack(LinkedList<int[]> l, int[][] ranges, int n, int j) {
             if (isCovered(n, l.getLast())) {
-                minCounter = Math.min(minCounter, counter);
+                return 1;
             }
+            Integer min = null;
             for (int i = j; i <= n; i++) {
                 if (l.getLast()[1] < ranges[i][0]) {
                     break;
                 }
                 int last = l.getLast()[1];
                 l.getLast()[1] = Math.max(last, ranges[i][1]);
-                backtrack(l, ranges, n, i + 1, counter + 1);
+                Integer current = backtrack(l, ranges, n, i + 1);
                 l.getLast()[1] = last;
+                if (current != null) {
+                    min = Math.min(min == null ? Integer.MAX_VALUE : min, current + 1);
+                }
             }
+            return min;
         }
 
         private boolean isCovered(int n, int[] interval) {
