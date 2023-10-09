bd9ba32159765f400ebdc1c86555431b31707dbc
Vladislav Dankevich <vielendanke1991@gmail.com>
2023-10-09 09:22:22 +0600

    daily task

---
+++ leetcode/binary_search/medium/FindFirstAndLastPositionOfElementInSortedArray_34.java
@@ -0,0 +1,30 @@
+package leetcode.binary_search.medium;
+
+import java.util.Arrays;
+
+public class FindFirstAndLastPositionOfElementInSortedArray_34 {
+
+    static class Solution {
+        public int[] searchRange(int[] nums, int target) {
+            int[] result = new int[]{-1, -1};
+
+            int idx = Arrays.binarySearch(nums, target);
+
+            if (idx < 0) {
+                return result;
+            }
+            int left = idx, right = idx;
+
+            while (true) {
+                if (left >= 0 && nums[left] == nums[idx]) {
+                    result[0] = left--;
+                } else if (right < nums.length && nums[right] == nums[idx]) {
+                    result[1] = right++;
+                } else {
+                    break;
+                }
+            }
+            return result;
+        }
+    }
+}
