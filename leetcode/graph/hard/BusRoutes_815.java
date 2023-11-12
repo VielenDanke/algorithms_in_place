b48e8c90af13e060740f525a72bd61d5e41b3a37
Vladislav Dankevich <vielendanke1991@gmail.com>
2023-11-12 10:00:35 +0600

    daily task

--- leetcode/graph/hard/BusRoutes_815.java
+++ leetcode/graph/hard/BusRoutes_815.java
@@ -1,37 +1,41 @@
 package leetcode.graph.hard;
 
-import java.util.ArrayDeque;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Queue;
+import java.util.*;
 
 public class BusRoutes_815 {
 
     private static class Solution {
         public int numBusesToDestination(int[][] routes, int source, int target) {
             int n = routes.length;
-            HashMap<Integer, HashSet<Integer>> destRoutes = new HashMap<>();
+            Map<Integer, Set<Integer>> destRoutes = new HashMap<>();
             for (int i = 0; i < routes.length; ++i) {
                 for (int j : routes[i]) {
                     destRoutes.putIfAbsent(j, new HashSet<>());
                     destRoutes.get(j).add(i);
                 }
             }
-            Queue<int[]> queue = new ArrayDeque<>();
-            queue.offer(new int[] {source, 0});
-            HashSet<Integer> visited = new HashSet<>();
+            Queue<int[]> queue = new LinkedList<>();
+            queue.offer(new int[]{source, 0});
+
+            Set<Integer> visited = new HashSet<>();
             visited.add(source);
+
             boolean[] visitedRoutes = new boolean[n];
+
             while (!queue.isEmpty()) {
-                int stop = queue.peek()[0], bus = queue.peek()[1];
-                queue.poll();
-                if (stop == target) return bus;
+                int[] nextRouteNode = queue.poll();
+                int stop = nextRouteNode[0], numberOfBus = nextRouteNode[1];
+                if (stop == target) {
+                    return numberOfBus;
+                }
                 for (int i : destRoutes.get(stop)) {
-                    if (visitedRoutes[i]) continue;
+                    if (visitedRoutes[i]) {
+                        continue;
+                    }
                     for (int j : routes[i]) {
                         if (!visited.contains(j)) {
                             visited.add(j);
-                            queue.offer(new int[] {j, bus + 1});
+                            queue.offer(new int[]{j, numberOfBus + 1});
                         }
                     }
                     visitedRoutes[i] = true;
