870882c08752449bafb6d466408ac4d08f615bd6
Vladislav Dankevich <vielendanke1991@gmail.com>
2023-12-01 10:15:42 +0600

    daily task

--- leetcode/strings/easy/golang_solution/check_if_two_string_arrays_are_equivalent_1662.go
+++ leetcode/strings/easy/golang_solution/check_if_two_string_arrays_are_equivalent_1662.go
@@ -1,7 +1,30 @@
 package golang_solution
 
-import "strings"
+import (
+	"strings"
+)
 
 func arrayStringsAreEqual(word1 []string, word2 []string) bool {
 	return strings.Join(word1, "") == strings.Join(word2, "")
+}
+
+func arrayStringsAreEqualNoExtraSpace(word1 []string, word2 []string) bool {
+	leftIndex, rightIndex, leftLetterIndex, rightLetterIndex := 0, 0, 0, 0
+
+	for leftIndex < len(word1) && rightIndex < len(word2) {
+		if word1[leftIndex][leftLetterIndex] != word2[rightIndex][rightLetterIndex] {
+			return false
+		}
+		leftLetterIndex++
+		rightLetterIndex++
+		if leftLetterIndex >= len(word1[leftIndex]) {
+			leftIndex++
+			leftLetterIndex = 0
+		}
+		if rightLetterIndex >= len(word2[rightIndex]) {
+			rightIndex++
+			rightLetterIndex = 0
+		}
+	}
+	return leftIndex == len(word1) && rightIndex == len(word2)
 }
