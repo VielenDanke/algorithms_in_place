b45412b911f5e985ed2209a08034fec4c34696f2
Vladislav Dankevich <vielendanke1991@gmail.com>
2023-12-10 17:00:58 +0600

    daily task

--- leetcode/strings/hard/golang_solutions/regular_expression_matching_10.go
+++ leetcode/strings/hard/golang_solutions/regular_expression_matching_10.go
@@ -2,14 +2,16 @@ package golang_solutions
 
 func isMatch(s string, p string) bool {
 	memo := make([][]bool, len(s)+1)
+	visited := make([][]bool, len(s)+1)
 	for i := range memo {
 		memo[i] = make([]bool, len(p)+1)
+		visited[i] = make([]bool, len(p)+1)
 	}
-	return dp(0, 0, s, p, memo)
+	return dp(0, 0, s, p, memo, visited)
 }
 
-func dp(i, j int, s, p string, memo [][]bool) bool {
-	if memo[i][j] {
+func dp(i, j int, s, p string, memo, visited [][]bool) bool {
+	if visited[i][j] {
 		return memo[i][j]
 	}
 	var result bool
@@ -20,11 +22,12 @@ func dp(i, j int, s, p string, memo [][]bool) bool {
 		isFirstMatch := i < len(s) && (p[j] == s[i] || p[j] == '.')
 
 		if j+1 < len(p) && p[j+1] == '*' {
-			result = dp(i, j+2, s, p, memo) || (isFirstMatch && dp(i+1, j, s, p, memo))
+			result = dp(i, j+2, s, p, memo, visited) || (isFirstMatch && dp(i+1, j, s, p, memo, visited))
 		} else {
-			result = isFirstMatch && dp(i+1, j+1, s, p, memo)
+			result = isFirstMatch && dp(i+1, j+1, s, p, memo, visited)
 		}
 	}
 	memo[i][j] = result
+	visited[i][j] = true
 	return memo[i][j]
 }
