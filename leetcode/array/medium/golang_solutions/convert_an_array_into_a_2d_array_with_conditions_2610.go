494f069c22e353d51f457c421a4d2c150749c06f
Vladislav Dankevich <vielendanke1991@gmail.com>
2024-01-02 13:13:13 +0600

    daily task

---
+++ leetcode/array/medium/golang_solutions/convert_an_array_into_a_2d_array_with_conditions_2610.go
@@ -0,0 +1,27 @@
+package golang_solutions
+
+func findMatrix(nums []int) [][]int {
+	arr := make([]int, 201)
+	for _, num := range nums {
+		arr[num]++
+	}
+	matrix := make([][]int, 0)
+
+	isRun := true
+
+	for isRun {
+		isRun = false
+		temp := make([]int, 0)
+		for num, counter := range arr {
+			if counter > 0 {
+				temp = append(temp, num)
+				arr[num]--
+				isRun = true
+			}
+		}
+		if isRun {
+			matrix = append(matrix, temp)
+		}
+	}
+	return matrix
+}
