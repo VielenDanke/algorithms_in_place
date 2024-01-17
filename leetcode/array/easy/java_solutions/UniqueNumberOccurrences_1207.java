55d576a41bcc0a16295a2c4d5a8833816663bd3e
Vladislav Dankevich <vielendanke1991@gmail.com>
2024-01-17 11:02:30 +0600

    daily task

--- leetcode/array/easy/java_solutions/UniqueNumberOccurrences_1207.java
+++ leetcode/array/easy/java_solutions/UniqueNumberOccurrences_1207.java
@@ -1,9 +1,6 @@
 package leetcode.array.easy.java_solutions;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class UniqueNumberOccurrences_1207 {
 
@@ -19,6 +16,45 @@ public class UniqueNumberOccurrences_1207 {
                 if (!visited.add(entry.getValue())) {
                     return false;
                 }
+            }
+            return true;
+        }
+    }
+
+    static class SolutionSort {
+        public boolean uniqueOccurrences(int[] arr) {
+            int[] counter = new int[2001];
+
+            for (int num : arr) {
+                counter[num + 1000]++;
+            }
+            Arrays.sort(counter);
+
+            for (int i = 1; i < counter.length; i++) {
+                if (counter[i] == counter[i - 1] && counter[i] > 0) {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+
+    static class SolutionArray {
+        private static final int ARRAY_MAX_SIZE = 2001;
+
+        public boolean uniqueOccurrences(int[] arr) {
+            int[] counter = new int[ARRAY_MAX_SIZE];
+
+            for (int num : arr) {
+                counter[num + 1000]++;
+            }
+            int[] unique = new int[ARRAY_MAX_SIZE];
+
+            for (int num : counter) {
+                if (num > 0 && unique[num] > 0) {
+                    return false;
+                }
+                unique[num]++;
             }
             return true;
         }
